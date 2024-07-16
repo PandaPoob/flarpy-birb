40679dd13e22f1633fa685c41f33aff0ba9d9f99
Freja Smith <frejasmith123@gmail.com>
2024-07-16 18:05:12 +0200

    initial commit

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 8;
+    public float deadZone = -45;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        
+        if(transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
+    }
+}
